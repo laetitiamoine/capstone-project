3ad3a230a46e65726f3d536bcdebc619dfa9ccee
Fellipe Marcellino <ffcmarcellino@gmail.com>
2020-02-11 18:38:27 -0800

    Add helper functions in OTM4RL

--- src/otm/OTM4RL.py
+++ src/otm/OTM4RL.py
@@ -8,18 +8,25 @@ class OTM4RL:
     def __init__(self, configfile, jaxb_only=False):
         self.otmwrapper = OTMWrapper(configfile)
 
+    def reset_queue(queue_dictionary):
+        pass
+
+    def set_signals(signal_dictionary):
+        pass
+
+    def get_queues(self):
+        pass
+
     def run_simulation(self,duration,output_dt):
     	self.otmwrapper.run_simple(start_time=0., duration=duration, output_dt=output_dt)
         self.Y = self.otmwrapper.get_state_trajectory()
-
-    def env_dynamics(self, state, action):
-        # Return next state and reward
-        pass
 
 class otmEnvDiscrete:
 
     def __init__(self, env_info, configfile):
 
+        self.num_actions = env_info.num_actions
+        self.num_states = env_info.num_states
         self.action_space = spaces.Discrete(env_info.num_actions)
         self.observation_space = spaces.Discrete(env_info.num_states)
         self.otm = OTM4RL(configfile)
@@ -29,8 +36,12 @@ class otmEnvDiscrete:
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def reset(self, network_config):
-        self.state = self.np_random.choice(env_info.num_states)
+    def set_state(self, state):
+        # start otm with state
+        self.state = state
+
+    def reset(self):
+        self.set_state(#random state)
         return self.state
 
     def step(self, action):
