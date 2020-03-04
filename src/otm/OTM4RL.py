6e12a814bebad4f2fca33a7b9c39bceea5125f8c
Gabriel Gomes <gomes@berkeley.edu>
2020-03-03 16:45:31 -0800

    controller info

--- src/otm/OTM4RL.py
+++ src/otm/OTM4RL.py
@@ -15,6 +15,9 @@ class OTM4RL:
         cfg = os.path.join(root_folder,'cfg', configfile)
         self.otmwrapper = OTMWrapper(cfg)
 
+    def run_simulation(self,duration,output_dt):
+        self.otmwrapper.run_simple(start_time=0., duration=duration, output_dt=output_dt)
+
     def get_link_ids(self):
         return self.otmwrapper.otm.scenario().get_link_ids()
 
@@ -35,44 +38,72 @@ class OTM4RL:
             max_queues[link_id] = link.get_jam_density_vpkpl() * link.getFull_length() * link.getFull_lanes() / 1000
         return max_queues
 
-    # returns a list of controllers
-    # each controller is a dictionary with
-    #   id
-    #   list of stages
-    #       each stage is a dictionary with
-    #       order
-    #       phases (list of phase ids)
-    def get_signal_controller_info(self):
-        X = [] # list of dictionary
-        # for controller in self.otmwrapper.otm.scenario().get_controllers():
-        #     if str(controller.getType())=='sig_pretimed':
-        #         print(controller)
-        #         print(controller.getPretimed_signal_info())
-        #         schedule = controller.getPretimed_signal_info().getSchedule()
-        #         stagelist = schedule[0].getStages()
-        #         # cntrl = {}
-        #         # cntrl['id'] = int(controller.getId())
-        #         # cntrl['stages'] = []
-
-
-        # X = [
-        #     {COMPLETE THIS!!!}}
-        # ]
-
-
-        return X
-
-    def get_roadconnection_info(self, phase_id):
-        #return [{roadconnection_id: ..., in_link: ..., out_link: ...}, {...}]
-        pass
-
     def set_queues(self,queue_dictionary):
         for link_id, q4link in queue_dictionary.items():
             self.otmwrapper.otm.scenario().set_link_vehicles(link_id,int(q4link['waiting']),int(q4link['transit']))
 
+    # returns map from signal id to object
+    # each controller is a dictionary with
+    #   list of stages
+    #       each stage is a dictionary with
+    #       order
+    #       phases (list of phase ids)
+    # def get_signal_info(self):
+    #     X = {} # id to controller
+    #     for ocntrl in self.otmwrapper.otm.scenario().get_controllers():
+    #         if str(ocntrl.getType())=='sig_pretimed':
+
+    #             pcntrl = {}
+    #             pcntrl['stages'] = []
+
+    #             # print(controller)
+    #             for sch in ocntrl.getSchedule():
+    #                 # print(sch.getCycle())
+    #                 # print(sch.getOffset())
+    #                 # print(sch.getStart_time())
+    #                 for stage in sch.getStages():
+    #                     print(stage.getOrder())
+    #                     print(stage.getDuration())
+    #                     print(stage.getCycle_starttime())
+
+    #             X[ocntrl.getId()] = pcntrl
+
+    # returns map from controller id to an controller object (a dict)
+    # cntrl['cycle']
+    # cntrl['offset']
+    # cntrl['stages'] an ordered list of stage dicts:
+    #   stage['duration']
+    #   stage['phases']
+    # ordered list of stages
+    # each stage is a dictionary with phases (a set) and duration (a float)
+    def get_controller_info(self):
+        X = {} # id to controller
+        for ocntrl in self.otmwrapper.otm.scenario().get_controllers():
+            if str(ocntrl.getType())=='sig_pretimed':
+
+                cntrl = {}
+
+                stages = []
+                for schitem in ocntrl.getSchedule():
+                    cntrl['cycle'] = schitem.getCycle()
+                    cntrl['offset'] = schitem.getOffset()
+                    for ostage in schitem.getStages():
+                        stage = {}
+                        stage['duration'] = ostage.getDuration()
+                        stage['phases'] = ostage.getPhases()
+                        stages.append(stage)
+
+                cntrl['stages'] = stages
+                X[ocntrl.getId()] = cntrl
+
+        return X
+
     def set_signals(self,signal_command):
         self.otmwrapper.otm.scenario().set_signal_stage(signal_command['id'],signal_command['green_stage_order'])
 
+    def get_signals(self):
+        pass
 
-    def run_simulation(self,duration):
-        self.otmwrapper.run_simple(start_time=0., duration=duration, output_dt=duration)
+    def get_roadconnection_info(self, phase_id):
+        #return [{roadconnection_id: ..., in_link: ..., out_link: ...}, {...}]
+        pass
--- src/otm/tests.py
+++ src/otm/tests.py
@@ -37,15 +37,65 @@ def test_get_queues():
 	while(time<duration):
 		otm4rl.otmwrapper.otm.advance(advance_time)
 		queues = otm4rl.get_queues()
-		print(queues)
 		time += advance_time;
 
 	# always end by deleting the wrapper
 	del otm4rl
 
-# note done
-def test_set_signals():
+# done
+def test_get_max_queues():
+	otm4rl = get_otm4rl()
+	del otm4rl
 
+# done
+def test_set_queues():
+	otm4rl = get_otm4rl()
+
+	# create random map
+	max_queues = otm4rl.get_max_queues()
+	myqueues = { link_id:{'waiting':0,'transit':0} for link_id in otm4rl.get_link_ids()}
+	for link_id in myqueues.keys():
+		total_queue = np.random.random()*max_queues[link_id]
+		p = np.random.random()
+		myqueues[link_id]['waiting'] = round(p*total_queue)
+		myqueues[link_id]['transit'] = round((1-p)*total_queue)
+
+	advance_time = 60.
+
+	otm4rl.otmwrapper.initialize(float(0))
+
+	otm4rl.otmwrapper.otm.advance(advance_time)
+
+	print("Before: ", otm4rl.get_queues()[26]['transit'])
+	print("Desired: ",myqueues[26]['transit'])
+
+	otm4rl.set_queues(myqueues)
+
+	print("After: ",otm4rl.get_queues()[26]['transit'])
+
+	# always end by deleting the wrapper
+	del otm4rl
+
+# RETURNS EMPTY
+def test_get_controller_info():
+	otm4rl = get_otm4rl()
+	X = otm4rl.get_controller_info()
+
+
+	print('cycle',X[2]['cycle'])
+	print('offset',X[2]['offset'])
+	for i,stage in enumerate(X[2]['stages']):
+		print(i,'duration',stage['duration'])
+		print(i,'phases',stage['phases'])
+
+	del otm4rl
+
+# TO DO
+def test_get_signals():
+	pass
+
+# FAILS
+def test_set_signals():
 
 	# my_signals[controller_id] = green stage id
 	my_signals = {}
@@ -73,48 +123,6 @@ def test_set_signals():
 
 	del otm4rl
 
-# done
-def test_get_max_queues():
-	otm4rl = get_otm4rl()
-	print(otm4rl.get_max_queues())
-	del otm4rl
-
-# not done
-def test_set_queues():
-	otm4rl = get_otm4rl()
-
-	# create random map
-	max_queues = otm4rl.get_max_queues()
-	myqueues = { link_id:{'waiting':0,'transit':0} for link_id in otm4rl.get_link_ids()}
-	print(myqueues)
-	for link_id in myqueues.keys():
-		total_queue = np.random.random()*max_queues[link_id]
-		p = np.random.random()
-		myqueues[link_id]['waiting'] = round(p*total_queue)
-		myqueues[link_id]['transit'] = round((1-p)*total_queue)
-
-	advance_time = 60.
-
-	otm4rl.otmwrapper.initialize(float(0))
-
-	otm4rl.otmwrapper.otm.advance(advance_time)
-
-	print(" ------- BEFORE ------------ ")
-	print( otm4rl.get_queues() )
-
-	otm4rl.set_queues(myqueues)
-
-	print(" ------- AFTER ------------ ")
-	print( otm4rl.get_queues() )
-
-	# always end by deleting the wrapper
-	del otm4rl
-
-# done
-def test_get_signal_controller_info():
-	otm4rl = get_otm4rl()
-	print(otm4rl.get_signal_controller_info())
-	del otm4rl
 
 if __name__ == '__main__':
-	print(test_set_queues())
+	print(test_get_controller_info())
